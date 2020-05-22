c79bf428f933a44b983fc773fcc096d7f76ed5ad
riku0389 <riku0389@gmail.com>
2020-05-22 19:52:14 +0800

    Thread,Runnable

---
+++ src/com/java/test/CurretThreadDemo.java
@@ -0,0 +1,21 @@
+package com.java.test;
+
+public class CurretThreadDemo {
+    public static void main(String[] args) {
+        Thread t = Thread.currentThread();
+
+        System.out.println("Current thread: " + t);
+
+        t.setName("My Thread");
+        System.out.println("After name change: " + t);
+
+        try {
+            for (int n = 5; n > 0; n--) {
+                System.out.println(n);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted");
+        }
+    }
+}
---
+++ src/com/java/test/ThreadDemo.java
@@ -0,0 +1,38 @@
+package com.java.test;
+class NewThread implements Runnable{
+    Thread t;
+
+    public NewThread() {
+        t=new Thread(this,"Demo Thread");
+        System.out.println("Child thread: "+t);
+        t.start();
+    }
+
+    @java.lang.Override
+    public void run() {
+        try{
+            for(int i=5;i>0;i--){
+                System.out.println("Child  Thread： "+i);
+                Thread.sleep(500);
+            }
+        }catch(InterruptedException e){
+            System.out.println("Child interrupted.");
+        }
+        System.out.println("Exitting child thread.");
+    }
+}
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+        new NewThread();
+        try{
+            for(int i=5;i>0;i--){
+                System.out.println("Main Thread: "+i);
+                Thread.sleep(1000);
+            }
+        }catch(InterruptedException e){
+            System.out.println("Main thread interrupted.");
+        }
+        System.out.println("Main thread interrupted");
+    }
+}
